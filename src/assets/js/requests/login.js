802293a0e9240eb2993ea77da86a1416c37083c7
Filippo Finke <afinke71@gmail.com>
2018-09-19 21:53:48 +0100

    Separated js and html

--- src/assets/js/requests/login.js
+++ src/assets/js/requests/login.js
@@ -2,6 +2,9 @@
  * Login function.
  * Check if the username and the password using i3albo API.
  *
+ * Last fix:
+ * Filippo Finke 19.09.2018
+ *
  * @author giuliobosco
  * @version 1.0
  */
@@ -9,7 +12,7 @@
 /**
  * Login in i3albo.
  */
-function login() {
+$( "#loginBtn" ).click(function() {
 	var username = $('#username').val();
 	var password = $('#password').val();
 
@@ -24,4 +27,4 @@ function login() {
 		.fail(function() {
 			$('#errorMessage').text("Server Error");
 		});
-}
+});
