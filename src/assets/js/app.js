0a6792561bcf827af9b425f18e55dc7afd9d6e0c
Giulio Bosco <giuliobva@gmail.com>
2018-09-20 15:25:42 +0200

    fix angular app - home, login

---
+++ src/assets/js/app.js
@@ -0,0 +1,43 @@
+/**
+ * Main i3albo app.
+ *
+ * @author giuliobosco
+ * @version 1.0
+ */
+
+/**
+ * Main i3albo app.
+ */
+var app = angular.module('ViewsAPP', ['ngRoute']);
+
+/**
+ * Configure app.
+ */
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			templateUrl: 'views/home.html'
+		})
+		.when('/login', {
+			templateUrl: 'views/login.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});
+
+/*
+app.config(function ($routeProvider) {
+	$routeProvider
+		.when('/', {
+			controller: 'HomeController',
+			templateUrl: 'views/home.html'
+		})
+		.when('/photos/:id', {
+			controller: 'PhotoController',
+			templateUrl: 'views/photo.html'
+		})
+		.otherwise({
+			redirectTo: '/'
+		});
+});*/
