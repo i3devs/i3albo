0f733c1ce4ac8fbcbb4afed18768a84b6e466bd8
Giulio Bosco <giuliobva@gmail.com>
2018-09-21 10:46:37 +0200

    add tasks.php - json example

---
+++ src/assets/php/app/tasks.php
@@ -0,0 +1,22 @@
+{
+	"tasks": [
+		{
+			"id": 1,
+			"subject": "M 226B - Java",
+			"date": "26/09/2018",
+			"time": "22:00",
+			"title": "MyPanelSample",
+			"author": "giuliobosco",
+			"decription": "http://classroom.google.com/c/MTc2MjgwOTc4NTda/a/MTc4NzI5MzYwMTFa/details",
+		},
+		{
+			"id": 1,
+			"subject": "M 226B - Java",
+			"date": "26/09/2018",
+			"time": "22:00",
+			"title": "MyPanelSample",
+			"author": "giuliobosco",
+			"decription": "http://classroom.google.com/c/MTc2MjgwOTc4NTda/a/MTc4NzI5MzYwMTFa/details",
+		}
+	]
+}
