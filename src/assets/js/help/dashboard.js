91f5e905409e5a6073f9dcbe5f4eec07f217dfbd
Giulio Bosco <giuliobva@gmail.com>
2018-09-22 21:55:05 +0200

    fix dashboard - functions doc

--- src/assets/js/help/dashboard.js
+++ src/assets/js/help/dashboard.js
@@ -1,3 +1,13 @@
+/**
+ * i3albo dashboard functions.
+ *
+ * @author giuliobosco
+ * @version 1.0
+ */
+
+/**
+ * preview class slide.
+ */
 $(document).on('click', '.preview', function () {
 	$(this).siblings(':last').slideToggle();
 });
