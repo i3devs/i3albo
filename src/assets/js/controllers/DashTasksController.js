72d57bd893f3031e99981e42459be4d3443f39ff
Giulio Bosco <giuliobva@gmail.com>
2018-09-22 22:06:03 +0200

    fix dashboard - DashTasksController

--- src/assets/js/controllers/DashTasksController.js
+++ src/assets/js/controllers/DashTasksController.js
@@ -1,4 +1,4 @@
-app.controller('DashTaskController', ['$scope', 'dashTasks', function($scope, dashTasks) {
+app.controller('DashTasksController', ['$scope', 'dashTasks', function($scope, dashTasks) {
 	dashTasks.success(function(data) {
 		$scope.dashTasks = data;
 		console.log(data);
